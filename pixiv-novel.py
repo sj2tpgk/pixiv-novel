6da1878fa30dbf5b89797a1657218b8ee2c8b8f0
a <a>
2024-08-04 09:08:30 +0900

    embed test.py inside main file

--- pixiv-novel.py
+++ pixiv-novel.py
@@ -1001,7 +1001,27 @@ def saveFile(name, text, maxLenBytes=os.pathconf('/', 'PC_NAME_MAX'), prefix="",
 ### test
 
 def test():
-    return
+    import subprocess as sp, time, urllib.error as ue, urllib.parse as up, urllib.request as ur
+    port = 8001
+    proc = sp.Popen(["python", "pixiv-novel.py", "-p", str(port), "-c", ""])
+
+    def test(path): # test http status and response length
+        time.sleep(1)
+        try:
+            res = ur.urlopen(f"http://localhost:{port}{path}")
+        except ue.HTTPError as e:
+            return print(f"FAIL {path}   ", e)
+        data = res.read()
+        if len(data) < 1000:
+            return print(f"FAIL {path}   ", "response too short", len(data))
+        print(f"OK   {path}")
+
+    try:
+        test("/")
+        test("/novel?id=15898879")
+        test("/search?q=" + up.quote("著作権フリー"))
+    finally:
+        proc.kill()
 
 
 ### Main
